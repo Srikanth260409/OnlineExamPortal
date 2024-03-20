c564747a9a5a954fe92eb14f7438331bff8b7b47
Srikanth260409 <162774663+Srikanth260409@users.noreply.github.com>
2024-03-20 11:17:13 +0530

    Add files via upload

---
+++ student/views.py
@@ -0,0 +1,166 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
+from django.shortcuts import render,redirect,reverse
+from . import forms,models
+from django.db.models import Sum
+from django.contrib.auth.models import Group, User
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required,user_passes_test
+from django.conf import settings
+from datetime import date, timedelta
+from exam import models as QMODEL
+from teacher import models as TMODEL
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+#for showing signup/login button for student
+def studentclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'student/studentclick.html')
+
+def student_signup_view(request):
+    userForm=forms.StudentUserForm()
+    studentForm=forms.StudentForm()
+    mydict={'userForm':userForm,'studentForm':studentForm}
+    if request.method=='POST':
+        userForm=forms.StudentUserForm(request.POST)
+        studentForm=forms.StudentForm(request.POST,request.FILES)
+        if userForm.is_valid() and studentForm.is_valid():
+            user=userForm.save()
+            user.set_password(user.password)
+            user.save()
+            student=studentForm.save(commit=False)
+            student.user=user
+            student.save()
+            my_student_group = Group.objects.get_or_create(name='STUDENT')
+            my_student_group[0].user_set.add(user)
+            recipient_list = ['saisatyapradeep@gmial.com']
+            subject="BY THIS MAIL A USER IS LOGGED IN PLEASE VERIFY THE LOGIN"
+            message='Thank you for registering on our website!'
+            from_email='satyanukala.24@gmail.com'
+            send_mail(subject, message, from_email, recipient_list)
+        return HttpResponseRedirect('studentlogin')
+    return render(request,'student/studentsignup.html',context=mydict)
+
+def is_student(user):
+    return user.groups.filter(name='STUDENT').exists()
+
+@login_required(login_url='studentlogin')
+@user_passes_test(is_student)
+def student_dashboard_view(request):
+    dict={
+    
+    'total_course':QMODEL.Course.objects.all().count(),
+    'total_question':QMODEL.Question.objects.all().count(),
+    }
+    return render(request,'student/student_dashboard.html',context=dict)
+
+@login_required(login_url='studentlogin')
+@user_passes_test(is_student)
+def student_exam_view(request):
+    courses=QMODEL.Course.objects.all()
+    return render(request,'student/student_exam.html',{'courses':courses})
+
+@login_required(login_url='studentlogin')
+@user_passes_test(is_student)
+def take_exam_view(request,pk):
+    course=QMODEL.Course.objects.get(id=pk)
+    total_questions=QMODEL.Question.objects.all().filter(course=course).count()
+    questions=QMODEL.Question.objects.all().filter(course=course)
+    total_marks=0
+    for q in questions:
+        total_marks=total_marks + q.marks
+    
+    return render(request,'student/take_exam.html',{'course':course,'total_questions':total_questions,'total_marks':total_marks})
+
+@login_required(login_url='studentlogin')
+@user_passes_test(is_student)
+def start_exam_view(request,pk):
+    course=QMODEL.Course.objects.get(id=pk)
+    questions=QMODEL.Question.objects.all().filter(course=course)
+    if request.method=='POST':
+        pass
+    response= render(request,'student/start_exam.html',{'course':course,'questions':questions})
+    response.set_cookie('course_id',course.id)
+    return response
+
+
+@login_required(login_url='studentlogin')
+@user_passes_test(is_student)
+def calculate_marks_view(request):
+    if request.COOKIES.get('course_id') is not None:
+        course_id = request.COOKIES.get('course_id')
+        course=QMODEL.Course.objects.get(id=course_id)
+        
+        total_marks=0
+        questions=QMODEL.Question.objects.all().filter(course=course)
+        for i in range(len(questions)):
+            
+            selected_ans = request.COOKIES.get(str(i+1))
+            actual_answer = questions[i].answer
+            if selected_ans == actual_answer:
+                total_marks = total_marks + questions[i].marks
+        student = models.Student.objects.get(user_id=request.user.id)
+        result = QMODEL.Result()
+        result.marks=total_marks
+        result.exam=course
+        result.student=student
+        result.save()
+
+        return HttpResponseRedirect('view-result')
+
+
+
+@login_required(login_url='studentlogin')
+@user_passes_test(is_student)
+def view_result_view(request):
+    courses=QMODEL.Course.objects.all()
+    return render(request,'student/view_result.html',{'courses':courses})
+    
+
+@login_required(login_url='studentlogin')
+@user_passes_test(is_student)
+def check_marks_view(request,pk):
+    course=QMODEL.Course.objects.get(id=pk)
+    student = models.Student.objects.get(user_id=request.user.id)
+    results= QMODEL.Result.objects.all().filter(exam=course).filter(student=student)
+    return render(request,'student/check_marks.html',{'results':results})
+
+@login_required(login_url='studentlogin')
+@user_passes_test(is_student)
+def student_marks_view(request):
+    courses=QMODEL.Course.objects.all()
+    return render(request,'student/student_marks.html',{'courses':courses})
+
+
+from django.shortcuts import render
+
+def graph_view(request):
+    data = [
+        {'field1': '1', 'field2': 40},
+        {'field1': '2', 'field2': 30},
+        {'field1': '3', 'field2': 50},
+    ]
+
+    return render(request, 'student/graph.html', {'data': data})
+
+
+class StudentUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if len(password1) < 8:
+            raise ValidationError("Password must be at least 8 characters long.")
+        return password1
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if "@" not in email:
+            raise ValidationError("Email must contain the '@' symbol.")
+        return email
